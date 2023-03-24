55ac2b7781a05a42a386b3f9a56548d70921968e
Racquel Glickman <racquel.glickman@gmail.com>
2023-03-24 14:24:28 -0400

    complete sending data lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData (userName, userEmail) {
+    let url = 'http://localhost:3000/users';
+    
+    const formData = {
+        name: userName,
+        email: userEmail,
+    }
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }, 
+        body: JSON.stringify(formData),
+    }
+
+    let body = document.querySelector('body');
+
+    return fetch(url, configurationObject)
+        .then((response) => response.json())
+        .then((data) => {
+            const userIdElement = document.createElement('h1')
+            userIdElement.textContent = data.id;
+            body.append(userIdElement);
+        })
+        .catch((error) => {
+            const errorMessage = document.createElement('p')
+            errorMessage.textContent = error.message;
+            body.append(errorMessage);
+        });
+    
+}
+
